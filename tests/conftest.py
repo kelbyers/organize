5e9dd4332e78a20c81b66b902a9670226d4a96ef
Kel Byers <ruferto@gmail.com>
2026-06-18 16:47:39 -0500

    add file timestamp support in 'make_files' and 'read_files', for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,24 @@
 from collections import Counter
+import os
 from pathlib import Path
-from typing import Dict, Iterable, List, Union
+from typing import Dict, Iterable, Union
 
 import pytest
 
 from organize.output import SavingOutput
+
+# define types for the make_files structure
+## just the raw contents of a file
+FileContentsRaw = Union[str, bytes] 
+## file contents plus a timestamp
+FileContentsTimeStamp = tuple["FileContentsRaw", float] 
+## when passing or returning files, use just the data, or can add timestamps
+FileContents = Union[FileContentsRaw, FileContentsTimeStamp]
+## A FileSpec is a dictionary of:
+##   - a file name plus the FileContents
+##   - OR a directory name, and a FileSpec that specifies files and subdirs
+FileSpec = Dict[str, Union[FileContents, "FileSpec"]]
+
 
 ORGANIZE_DIR = Path(__file__).parent.parent
 
@@ -18,7 +32,7 @@ def equal_items(a: Iterable, b: Iterable) -> bool:
     return Counter(a) == Counter(b)
 
 
-def make_files(structure: Union[Dict, List], path: Union[Path, str] = "."):
+def make_files(structure: Union[FileSpec, list[str]], path: Union[Path, str] = "."):
     """Example structure:
 
     {
@@ -30,10 +44,28 @@ def make_files(structure: Union[Dict, List], path: Union[Path, str] = "."):
         },
         "file.txt": "Hello world\nAnother line",
     }
+
+    Or, with timestamps, where the timstamps are POSIX file timestamps (see 
+    os.utime); it is technically possible to only specify timestamps for some
+    files and no timestamp for others, but the `read_files*()` function will
+    include all timestamps or no timestamps:
+
+    {
+        "folder": {
+            "subfolder": {
+                "empty.txt": ("", 1781818225.400407),
+                "other.pdf": ("text", 1781818321.400407),
+            },
+        },
+    }
     """
     if isinstance(path, str):
         path = Path(path)
     path.mkdir(parents=True, exist_ok=True)
+
+    # declare the typing specs
+    name: str
+    content: Union[FileContents, FileSpec]
 
     # structure is a list of filenames
     if isinstance(structure, list):
@@ -44,6 +76,11 @@ def make_files(structure: Union[Dict, List], path: Union[Path, str] = "."):
     # structure is a dict
     for name, content in structure.items():
         resource: Path = path / name
+
+        timestamp : Union[float, None] = None
+
+        if isinstance(content, tuple):
+            content, timestamp = content
 
         # folders are dicts
         if isinstance(content, dict):
@@ -58,16 +95,26 @@ def make_files(structure: Union[Dict, List], path: Union[Path, str] = "."):
             resource.write_text(content)
         else:
             raise ValueError(f"Unknown file data {content}")
+        
+        # if a timestamp was provided, set st_atime and st_mtime; we only care
+        # about st_mtime, but utime requires both values
+        if timestamp is not None:
+            os.utime(resource, (timestamp, timestamp))
 
 
-def read_files(path: Union[Path, str] = "."):
+
+def read_files(path: Union[Path, str] = ".", with_timestamp: bool = False):
     if isinstance(path, str):
         path = Path(path)
 
-    result = dict()
+    result: FileSpec = dict()
     for x in path.glob("*"):
         if x.is_file():
-            result[x.name] = x.read_text()
+            if with_timestamp:
+                # timestamp requested, so include it in the results
+                result[x.name] = (x.read_text(), x.stat().st_mtime)
+            else:
+                result[x.name] = x.read_text()
         if x.is_dir():
-            result[x.name] = read_files(x)
+            result[x.name] = read_files(x, with_timestamp)
     return result
--- tests/test_make_files.py
+++ tests/test_make_files.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pathlib import Path
+
 from conftest import make_files, read_files
 
 
@@ -14,3 +17,29 @@ def test_make_files(fs):
 
     make_files(files, "test")
     assert read_files("test") == files
+
+def test_make_files_from_list(fs):
+    names = ["asd.txt", "newname 2.pdf", "newname.pdf", "test.pdf"]
+    make_files(names, "test")
+
+    for name in names:
+        assert (Path("test") / name).exists()
+
+
+def test_make_files_with_timestamp(fs):
+    ts1 = datetime(2020, 1, 1).timestamp()
+    ts2 = datetime(2020, 1, 2).timestamp()
+    ts3 = datetime(2020, 1, 3).timestamp()
+
+    files = {
+        "folder": {
+            "subfolder": {
+                "test.txt": ("", ts2),
+                "other.pdf": ("text", ts3),
+            },
+        },
+        "foo.txt": ("content", ts1),
+    }
+
+    make_files(files, "test")
+    assert read_files("test", True) == files
